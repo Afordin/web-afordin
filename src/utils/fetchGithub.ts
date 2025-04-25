00c9116070383c16cbae46f0f0a3dbafe65426ec
octavio.pincha <octavio.pincha@gmail.com>
2025-04-25 06:03:49 +0000

    chore(fetchGithub): add type in contributors variabale

--- src/utils/fetchGithub.ts
+++ src/utils/fetchGithub.ts
@@ -1,9 +1,9 @@
 import type { Contributor } from '@/types/index.d'
 
-export const fetchGithub = async (repo: string): Promise<Contributor[]> => {
+export const fetchGithub = async (repo: string): Promise<Array<Contributor>> => {
   try {
     const response = await fetch(`https://api.github.com/repos/${repo}/contributors`)
-    const contributors = await response.json()
+    const contributors = (await response.json()) as Promise<Array<Contributor>>
     return contributors
   } catch (error) {
     console.error('Error fetching data:', error)
